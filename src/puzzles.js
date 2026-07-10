eb39bd14a2c38b1eea3a2a21fb9939b0eb30dde1
DaveZajicek5 <77385685+DaveZajicek5@users.noreply.github.com>
2026-07-10 13:19:28 +0200

    Add first imported Queens puzzle

---
+++ src/puzzles.js
@@ -0,0 +1,22 @@
+export const PUZZLES = [
+  {
+    "id": "zipgame-day-583",
+    "source": "zipgameonline",
+    "day": 583,
+    "size": 8,
+    "capturedAt": "2025-12-04T09:04:02",
+    "regions": "0000112200001122000011110033311144433333444435556644357766443555",
+    "solution": "...Q..........Q.....Q.....Q.....Q............Q.........Q.Q......",
+    "solutionCols": [
+      4,
+      7,
+      5,
+      3,
+      1,
+      6,
+      8,
+      2
+    ],
+    "url": "https://zipgameonline.com/linkedin-queens-answers/day-583"
+  }
+];
