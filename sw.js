8edad3a9cc9d6d5cfffaee606bbcb02133629797
DaveZajicek5 <77385685+DaveZajicek5@users.noreply.github.com>
2026-07-20 16:33:18 +0200

    Bump offline cache for guided archive mode

---
+++ sw.js
@@ -0,0 +1,57 @@
+const CACHE = "queens-offline-v9";
+const CORE = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon.svg",
+  "./src/app.js",
+  "./src/puzzles.js",
+  "./src/generator-beta.js",
+  "./src/training.js",
+  "./src/archive-random.js",
+  "./src/drag-input.js",
+  "./src/pwa.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE).then(cache => cache.put("./index.html", copy));
+          return response;
+        })
+        .catch(() => caches.match("./index.html", { ignoreSearch: true }).then(response => response || caches.match("./", { ignoreSearch: true })))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request, { ignoreSearch: true }).then(cached => {
+      const network = fetch(event.request)
+        .then(response => {
+          if (response.ok) caches.open(CACHE).then(cache => cache.put(event.request, response.clone()));
+          return response;
+        })
+        .catch(() => cached);
+      return cached || network;
+    })
+  );
+});
